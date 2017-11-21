b4b4f31638a4ff9ee744c37f015dfef8ea730abc
snowman2010 <west.south190011@gmail.com>
2017-11-21 08:31:22 +0100

    update create single page

--- src/app/accounts/create-single/create-single.component.ts
+++ src/app/accounts/create-single/create-single.component.ts
@@ -6,7 +6,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-single.component.css']
 })
 export class CreateSingleComponent implements OnInit {
-
+  private fields = [
+    {
+      name: 'AKA Name',
+      id: 'aka',
+      type: 'input'
+    },
+    {
+      name: 'Legal Name',
+      id: 'legalName',
+      type: 'input'
+    },
+    {
+      name: 'Short Code',
+      id: 'shortCode',
+      type: 'input'
+    },
+    {
+      name: 'Email Domain',
+      id: 'emailDomain',
+      type: 'input'
+    },
+    {
+      name: 'Web Domain',
+      id: 'webDomain',
+      type: 'input'
+    },
+    {
+      name: 'Legal Address',
+      id: 'legalAddress',
+      type: 'textarea'
+    }
+  ]
   constructor() { }
 
   ngOnInit() {
