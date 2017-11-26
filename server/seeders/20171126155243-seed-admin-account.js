843be0b1f4adbf4b3ad8e11caaad0a2a6ca12119
Anastasiya Shtunder <fresh.vns16@outlook.com>
2017-11-27 01:42:07 +0200

    fix migration and seeder

--- server/seeders/20171126155243-seed-admin-account.js
+++ server/seeders/20171126155243-seed-admin-account.js
@@ -2,25 +2,12 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkInsert('Person', [{
-        name: 'John Doe',
-        isBetaMember: false
-      }], {});
-    */
+    return queryInterface.bulkInsert('flm_contacts', [
+      { primaryEmail: 'admin@finloom.com', password: '$2a$08$d/YAW4BBHR0V662/eaAkLeDh9syggrTPaNUFLXWsSwIwPuyD1wes6'}
+    ], {});
   },
 
   down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkDelete('Person', null, {});
-    */
+    return queryInterface.bulkDelete('flm_contacts', null, {});
   }
 };
