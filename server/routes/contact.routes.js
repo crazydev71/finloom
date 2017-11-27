54082c6389d54479101ad12d21afed8a7dab54ec
Crazy Dev <crazy.dev@aol.com>
2017-11-27 02:22:01 -0800

    fixed banktype migration issue

--- server/routes/contact.routes.js
+++ server/routes/contact.routes.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const Contact = require('../models').Contact;
+const ContactList = require('../models').ContactList;
+const ContactEmails = require('../models').ContactEmails;
 const baseAPI = require('./base');
 
 router.use(baseAPI('Contact'));
