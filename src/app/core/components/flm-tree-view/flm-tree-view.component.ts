3ddc562ba4d630d314e2a0e4a7f0d29082a49515
Anastasiya Shtunder <fresh.vns16@outlook.com>
2017-11-29 22:01:27 +0200

    build component

--- src/app/core/components/flm-tree-view/flm-tree-view.component.ts
+++ src/app/core/components/flm-tree-view/flm-tree-view.component.ts
@@ -15,27 +15,13 @@ export class FlmTreeViewComponent implements OnInit {
 
   items = [];
   isAnimated = true;
-  autoCollapse = true;
+  autoCollapse = false;
   expandOnClick = true;
   constructor(private dataservice: DataService) {
-    this.items = [
-      {
-        header: 'Electronics', img: 'assets/img/default-avatar.png', items: [
-          { header: 'Trimmers/Shavers' },
-          { header: 'Tablets' },
-          {
-            header: 'Phones', img: 'assets/img/default-avatar.png', items: [
-              { header: 'Apple' },
-              { header: 'Motorola', newItem: true },
-              { header: 'Nokia' },
-              { header: 'Samsung' }]
-          },
-          { header: 'Speakers', newItem: true },
-          { header: 'Monitors' }
-        ]
-      },
-    ];
-    // this.dataservice.getData('/api/account/hierarchy/')
+    this.items = [{header: 'Loading...', /* img: '*.png', items: [{}] */}];
+    this.dataservice.getData('/api/account/get/hierarchy/1').subscribe((data) => {
+      console.log(data);
+    })
   }
 
   ngOnInit() {
