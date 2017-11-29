7d65a1164973862c7aac5f57b23dbcdb381ba926
Anastasiya Shtunder <fresh.vns16@outlook.com>
2017-11-29 18:03:27 +0200

    fix css issue

--- src/app/accounts/my-account/my-account.component.ts
+++ src/app/accounts/my-account/my-account.component.ts
@@ -2,6 +2,7 @@ import * as wjcCore from 'wijmo/wijmo';
 import * as wjcNav from 'wijmo/wijmo.nav';
 
 import { Component, OnInit, EventEmitter, Input, Inject } from '@angular/core';
+import { DataService } from '../../_services/data.services';
 
 @Component({
   selector: 'app-my-account',
@@ -14,7 +15,7 @@ export class MyAccountComponent implements OnInit {
   isAnimated = true;
   autoCollapse = true;
   expandOnClick = true;
-  constructor() {
+  constructor(private dataservice: DataService) {
     this.items = [
       {
         header: 'Electronics', img: 'assets/img/default-avatar.png', items: [
@@ -32,6 +33,7 @@ export class MyAccountComponent implements OnInit {
         ]
       },
     ];
+    // this.dataservice.getData('/api/account/')
   }
 
   ngOnInit() {
