d4fa6e2cb6da3b8aea54275db0dcf67a72dd32d8
coolhuman <cool.human@outlook.com>
2017-12-01 22:30:19 +1000

    add api to get contactlist and delete contactlist

--- server/routes/contact_list.routes.js
+++ server/routes/contact_list.routes.js
@@ -2,20 +2,52 @@ const express = require('express');
 const router = express.Router();
 const ContactList = require('../models').ContactList;
 const baseAPI = require('./base');
-
-router.use(baseAPI('ContactList'));
+const ContactXListModel = require('../models')['ContactXList'];
 
 router.post('/:id', async (req, res) => {
   const {id} = req.params;
   const data = req.body;
-  const ContactXListModel = require('../models')['ContactXList'];
-  for (var i = 0; i < data.ContactIds.length; i++)
+  for (var i = 0; i < data.contactIds.length; i++)
   {
-    const idata = {contactId: data.ContactIds[i], contactListId: id};
+    const idata = {contactId: data.contactIds[i], contactListId: id};
     await ContactXListModel.create(idata, {include: [{all: true, nested: true}]});
   }
   return res.status(200).json({msg: "Success"});
 });
+
+router.get('/:id', async (req, res) => {
+  const {id} = req.params;
+  const item = await ContactList.findById(id);
+  const contacts = await ContactXListModel.findAll({where:{contactListId: id}});
+
+  var ret = item.toJSON();
+  ret.contacts = contacts;
+  if (item)
+    return res.json(ret);
+  else
+    return res.status(404).json({msg: "Invalid request"});
+});
+
+router.delete('/:id', async (req, res) => {
+  const {id} = req.params;
+  let item = await ContactList.findById(id);
+
+  const contacts = await ContactXListModel.findAll({where:{contactListId: id}});
+  for(var i = 0; i < contacts.length; i++)
+  {
+    let contact = await ContactXListModel.findById(contacts[i].id);
+    await contact.destroy();
+  }
+
+  if (item) {
+    await item.destroy();
+    res.json({deletedId: id});
+  } else {
+    res.status(404).json({msg: "Invalid request"});
+  }
+});
+
+router.use(baseAPI('ContactList'));
 
 module.exports = function (rootRouter) {
   rootRouter.use('/contact-list', router);
