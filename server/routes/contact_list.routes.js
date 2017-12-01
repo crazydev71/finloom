5d628fabfece1dcdfc14330e28253f0df983fa36
coolhuman <cool.human@outlook.com>
2017-12-01 20:36:07 +1000

    add api for contacts to contact list

--- server/routes/contact_list.routes.js
+++ server/routes/contact_list.routes.js
@@ -5,6 +5,18 @@ const baseAPI = require('./base');
 
 router.use(baseAPI('ContactList'));
 
+router.post('/:id', async (req, res) => {
+  const {id} = req.params;
+  const data = req.body;
+  const ContactXListModel = require('../models')['ContactXList'];
+  for (var i = 0; i < data.ContactIds.length; i++)
+  {
+    const idata = {contactId: data.ContactIds[i], contactListId: id};
+    await ContactXListModel.create(idata, {include: [{all: true, nested: true}]});
+  }
+  return res.status(200).json({msg: "Success"});
+});
+
 module.exports = function (rootRouter) {
-  rootRouter.use('/contact-list', router)
+  rootRouter.use('/contact-list', router);
 }
